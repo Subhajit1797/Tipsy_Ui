526b7a2c2ac946550f992e24991ff0f1690dc2b8
USER <user@01HW1963605.local>
2022-07-16 17:05:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tipsy';
+
+  form = this.fb.group({
+    name: ['', {
+      Validators: [
+
+      ]
+    }],
+    email: ['', {
+        validators: [
+            Validators.required, 
+            Validators.email
+        ],
+        updateOn: 'blur'
+    }],
+    password: ['', 
+        [
+          Validators.required, 
+          Validators.minLength(8),
+          // createPasswordStrengthValidator()
+        ]
+    ]
+  });  
+
+  constructor(private fb: FormBuilder) {}
+
+  get name() {
+      return this.form.controls['name'];
+  }
+
+  get email() {
+      return this.form.controls['email'];
+  }
+
+  get password() {
+      return this.form.controls['password'];
+  }
 }
